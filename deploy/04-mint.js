a53b4b49412b416a33c298a616c367fc156ff149
thefishingdock <jeffreymurphysimon@gmail.com>
2022-08-25 19:54:22 +0700

    initial commit

---
+++ deploy/04-mint.js
@@ -0,0 +1,24 @@
+const { network, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+
+module.exports = async function ({ getNamedAccounts }) {
+    const { deployer } = await getNamedAccounts()
+    const basicNft = await ethers.getContract("BasicNft", deployer)
+    const basicMintTx = await basicNft.mintNft()
+    await basicMintTx.wait(1)
+    console.log(`Basic NFT index 0 has tokenURI: ${await basicNft.tokenURI(0)}`)
+
+    const highValue = ethers.utils.parseEther("4000")
+    const dynamicSvgNft = await ethers.getContract("DynamicSvgNft", deployer)
+    const dynamicSvgNftMintTx = await dynamicSvgNft.mintNft(highValue)
+    await dynamicSvgNftMintTx.wait(1)
+    console.log(`Dynamic SVG NFT index 1 tokenURI: ${await dynamicSvgNft.tokenURI(1)}`)
+
+    const randomIpfsNft = await ethers.getContract("RandomIpfsNft", deployer)
+    const mintFee = await randomIpfsNft.getMintFee()
+    const randomIpfsNftMintTx = await randomIpfsNft.requestNft({ value: mintFee.toString() })
+    const randomIpfsNftMintTxReceipt = await randomIpfsNftMintTx.wait(1)
+    console.log(`Random IPFS NFT index 0 tokenURI: ${await randomIpfsNft.tokenURI(0)}`)
+}
+
+module.exports.tags = ["all", "mint"]
